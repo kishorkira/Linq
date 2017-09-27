76bdf7d05c02c462f71fbbd4136b9db985467261
kishorkira <kishorkira@gmail.com>
2017-09-27 17:30:14 +0530

    Show Large Files Without Linq

--- Basics/Program.cs
+++ Basics/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace Basics
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Basics
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            string path = @"C:\windows";
+            ShowLargeFilesWithoutLinq(path);
+
+        }
+
+        private static void ShowLargeFilesWithoutLinq(string path)
+        {
+            DirectoryInfo directory = new DirectoryInfo(path);
+            FileInfo[] files= directory.GetFiles();
+            Array.Sort(files, new FileInfoComparer());
+            for(int i=0;i<10;i++)
+            {
+                FileInfo file = files[i];
+                Console.WriteLine($"{file.Length,-15:N0} : {file.Name} ");
+            }
+        }
+    }
+    public class FileInfoComparer : IComparer<FileInfo>
+    {
+        public int Compare(FileInfo x, FileInfo y)
+        {
+            return y.Length.CompareTo(x.Length);
         }
     }
 }
