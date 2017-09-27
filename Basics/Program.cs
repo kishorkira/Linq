1f47339930999806add219cc489eee43b26939ef
kishorkira <kishorkira@gmail.com>
2017-09-27 17:40:29 +0530

    Show Large Files With Linq

--- Basics/Program.cs
+++ Basics/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Basics
 {
@@ -10,7 +11,30 @@ namespace Basics
         {
             string path = @"C:\windows";
             ShowLargeFilesWithoutLinq(path);
+            Console.WriteLine("**********");
+            ShowLargeFilesWithLinq(path);
 
+        }
+
+        private static void ShowLargeFilesWithLinq(string path)
+        {
+            var query = from file in new DirectoryInfo(path).GetFiles()
+                        orderby file.Length descending
+                        select file;
+            foreach(var file in query.Take(10))
+            {
+                Console.WriteLine($"{file.Length,-15:N0} : {file.Name} ");
+            }
+            Console.WriteLine("Method Syntax");
+
+            var query1 = new DirectoryInfo(path).GetFiles()
+                        .OrderByDescending(f => f.Length)
+                        .Take(10);
+                        
+            foreach (var file in query1)
+            {
+                Console.WriteLine($"{file.Length,-15:N0} : {file.Name} ");
+            }
         }
 
         private static void ShowLargeFilesWithoutLinq(string path)
