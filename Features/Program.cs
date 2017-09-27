36727430fe8db5e29712c6493bf811612f34e6dd
kishorkira <kishorkira@gmail.com>
2017-09-27 18:09:16 +0530

    IEnumerable,IEnumerator

--- Features/Program.cs
+++ Features/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Features
 {
@@ -6,19 +7,25 @@ namespace Features
     {
         static void Main(string[] args)
         {
-            Employee[] developers = new Employee[]
+            IEnumerable<Employee> developers = new Employee[]
             {
                 new Employee{Id=1,Name="Ram"},
                 new Employee{Id=2,Name="Adi"}
 
             };
 
-            List<Employee> sales = new List<Employee>
+            IEnumerable<Employee> sales = new List<Employee>
             {
                 new Employee{Id=3,Name="Raj"},
                 new Employee{Id=4,Name="Sri"}
 
             };
+
+            IEnumerator<Employee> enumerator = developers.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current.Name);
+            }
         }
     }
 }
