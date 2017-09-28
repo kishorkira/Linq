8e44cd8121559bb2219804d781d3955c4381f0ac
kishorkira <kishorkira@gmail.com>
2017-09-28 10:36:27 +0530

    Custom Linq Without Yield

--- Queries/Program.cs
+++ Queries/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Queries
 {
@@ -10,10 +12,18 @@ namespace Queries
             {
                 new Movie{Title="IT",Rating= 4.0f,Year=2017},
                 new Movie{Title="Spider-Man",Rating= 3.8f,Year=2017},
-                new Movie{Title="Nice Guys",Rating= 4.2f,Year=2016}
-
-
+                new Movie{Title="Nice Guys",Rating= 4.2f,Year=2016},
+                new Movie{Title="Fast8",Rating= 4.5f,Year=2016}
             };
+
+            foreach(var movie in movies.Filter(m => m.Year == 2016))
+            {
+                Console.WriteLine(movie.Title);
+            }
+            foreach (var movie in movies.Where(m => m.Year == 2016))
+            {
+                Console.WriteLine(movie.Title);
+            }
         }
     }
 }
