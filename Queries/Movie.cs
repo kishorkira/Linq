a27a6ba7cf6c2a34365d15e860b6cdccf2165441
kishorkira <kishorkira@gmail.com>
2017-09-28 09:54:03 +0530

    Queries project setup

---
+++ Queries/Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace Queries
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public float Rating { get; set; }
+        public int Year { get; set; }
+
+    }
+}
---
+++ Queries/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Queries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Movie> movies = new List<Movie>
+            {
+                new Movie{Title="IT",Rating= 4.0f,Year=2017},
+                new Movie{Title="Spider-Man",Rating= 3.8f,Year=2017},
+                new Movie{Title="Nice Guys",Rating= 4.2f,Year=2016}
+
+
+            };
+        }
+    }
+}
